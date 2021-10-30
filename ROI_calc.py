5a3dfb734d7e93c9e750f820f3767684e15d37ec
leda84 <laura.dejesus929@gmail.com>
2021-10-29 23:52:37 -0500

    made changes

--- ROI_calc.py
+++ ROI_calc.py
@@ -12,7 +12,7 @@ class ROICalc():
         while True:
             input("Hit ENTER to calculate monthly expenses. Enter 'q' at any time to quit.  ")
 
-            tax = input("How much is your monthly tax on the property? ")
+            tax = input("\nHow much is your monthly tax on the property? (round up to whole dollar) ")
             if tax.lower() == "q":
                 break
             elif tax.isdigit() == False:
@@ -20,7 +20,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            insurance = input("How much are you paying on insurance monthly? ")
+            insurance = input("\nHow much are you paying on insurance monthly? (round up to whole dollar) ")
             if insurance.lower() == "q":
                 break
             elif insurance.isdigit() == False:
@@ -28,7 +28,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
 
-            utilities = input("If your tenant will not be covering utilities, about how much will you pay per month? (round up to whole dollar) ")
+            utilities = input("\nIf your tenant will not be covering utilities, about how much will you pay per month? (round up to whole dollar) ")
             if utilities.lower() == "q":
                 break
             elif utilities.isdigit() == False:
@@ -36,7 +36,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
 
-            hoafees = input("How much are your monthly HOA fees? (enter 0 if not applicable) (round up to whole dollar) ")
+            hoafees = input("\nHow much are your monthly HOA fees? (enter 0 if not applicable) (round up to whole dollar) ")
             if hoafees.lower() == "q":
                 break
             elif hoafees.isdigit() == False:
@@ -44,7 +44,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            vacancy = input("How much will you be putting aside each month to cover for future vacancy? (round up to whole dollar) ")
+            vacancy = input("\nHow much will you be putting aside each month to cover for future vacancy? (round up to whole dollar) ")
             if vacancy.lower() == "q":
                 break
             elif vacancy.isdigit() == False:
@@ -52,7 +52,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            repairs = input("How much would you like to set aside monthly for future repairs? (round up to whole dollar) ")
+            repairs = input("\nHow much would you like to set aside monthly for future repairs? (round up to whole dollar) ")
             if repairs.lower() == "q":
                 break
             elif repairs.isdigit() == False:
@@ -60,7 +60,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            capex = input("How much would you like to put aside monthly for CapEx? new roof, windows, etc... (round up to whole dollar) ")
+            capex = input("\nHow much would you like to put aside monthly for CapEx? new roof, windows, etc... (round up to whole dollar) ")
             if capex.lower() == "q":
                 break
             elif capex.isdigit() == False:
@@ -68,7 +68,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            propmang = input("How much will you be spending monthly on property management? (round up to whole dollar) ")
+            propmang = input("\nHow much will you be spending monthly on property management? (round up to whole dollar) ")
             if propmang.lower() == "q":
                 break
             elif propmang.isdigit() == False:
@@ -76,7 +76,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            mortgage = input("How much is your monthly mortgage? (round up to whole dollar) ")
+            mortgage = input("\nHow much is your monthly mortgage? (round up to whole dollar) ")
             if mortgage.lower() == "q":
                 break
             elif mortgage.isdigit() == False:
@@ -86,13 +86,13 @@ class ROICalc():
             
             
             total = int(tax) + int(insurance) + int(utilities) + int(hoafees) + int(vacancy) + int(repairs) + int(capex) + int(propmang) + int(mortgage)
-            print(f"Your total monthly expenses are: ${total}")
+            print(f"\nYour total monthly expenses are: ${total} \n")
             break
 
 
     def cashflow(self):
         while True:
-            answer = input("To calculate cash flow, you will need to know your monthly expenses." "\nDo you know your monthly expenses? (yes/no or 'q' to quit) ")
+            answer = input("\nTo calculate cash flow, you will need to know your monthly expenses." "\nDo you know your monthly expenses? (yes/no or 'q' to quit) ")
             if answer.lower() == 'q':
                 break
             elif answer.lower() == 'no':
@@ -103,7 +103,7 @@ class ROICalc():
             else: # used in case they input anything other than 'q' 'yes' or 'no'
                 continue
 
-            expenses = input("How much are your monthly expenses? (round up to whole dollar) ") 
+            expenses = input("\nHow much are your monthly expenses? (round up to whole dollar) ") 
             if expenses.lower() == "q":
                 break
             elif expenses.isdigit() == False:
@@ -111,7 +111,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
 
-            rent = input("How much will you be charging for rent on the property? (round up to whole dollar) ")
+            rent = input("\nHow much will you be charging for rent on the property? (round up to whole dollar) ")
             if rent.lower() == "q":
                 break
             elif rent.isdigit() == False:
@@ -121,13 +121,13 @@ class ROICalc():
 
             cashflow = int(rent) - int(expenses)
 
-            print(f"Your monthly cashflow is: ${cashflow}")
+            print(f"\nYour monthly cashflow is: ${cashflow} \n")
             break
             
         
     def roi(self):
         while True:
-            answer = input("To calculate ROI, you will need to know your monthly cashflow." "\nDo you know your monthly cashflow? (yes/no or 'q' to quit) ")
+            answer = input("\nTo calculate ROI, you will need to know your monthly cashflow." "\nDo you know your monthly cashflow? (yes/no or 'q' to quit) ")
             if answer.lower() == 'q':
                 break
             elif answer.lower() == 'no':
@@ -138,7 +138,7 @@ class ROICalc():
             else: # used in case they input anything other than 'q' 'yes' or 'no'
                 continue
 
-            cashflow = input("Enter your monthly cash flow (round up to whole dollar): ")
+            cashflow = input("\nEnter your monthly cash flow (round up to whole dollar): ")
             if cashflow.lower() == "q":
                 break
             elif cashflow.isdigit() == False:
@@ -146,7 +146,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            downPayment = input("Enter your down payment (round up to whole dollar): ")
+            downPayment = input("\nEnter your down payment (round up to whole dollar): ")
             if downPayment.lower() == "q":
                 break
             elif downPayment.isdigit() == False:
@@ -154,7 +154,7 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            closing = input("Enter your closing costs (round up to whole dollar): ")
+            closing = input("\nEnter your closing costs (round up to whole dollar): ")
             if closing.lower() == "q":
                 break
             elif closing.isdigit() == False:
@@ -162,15 +162,28 @@ class ROICalc():
                 # TO TAKE USER BACK TO ENTER A NUMBER
                 continue
             
-            rehab = input("Enter your rehab budget (round up to whole dollar): ")
-            misc = input("Enter any other miscellaneous investments to property (round up to whole dollar): ")
-
+            rehab = input("\nEnter your rehab budget (round up to whole dollar): ")
+            if rehab.lower() == "q":
+                break
+            elif rehab.isdigit() == False:
+                print("Please enter a number")
+                # TO TAKE USER BACK TO ENTER A NUMBER
+                continue
+            
+            misc = input("\nEnter any other miscellaneous investments to property (round up to whole dollar): ")
+            if misc.lower() == "q":
+                break
+            elif misc.isdigit() == False:
+                print("Please enter a number")
+                # TO TAKE USER BACK TO ENTER A NUMBER
+                continue
+            
             total_investment = int(downPayment) + int(closing) + int(rehab) + int(misc)
-            print(f"Your total investment is: ${total_investment}")
+            print(f"\nYour total investment is: ${total_investment}")
             annual_cashflow = int(cashflow) * 12
             print(f"Your annual cashflow is: ${annual_cashflow}")
             roi = (annual_cashflow/total_investment) * 100
-            print(f"Your Cash on Cash Return On Investment is: {roi}%")
+            print(f"Your Cash on Cash Return On Investment is: {roi}% per year \n")
             break
 
 
